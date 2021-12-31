9d210782e74ebf8dbc57b11a25616796d50e77f7
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2021-12-31 11:43:42 +0100

    + Upload project

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import * as fs from "fs";
+import morgan from "morgan";
+import "dotenv/config";
+
+const app = express();
+const port = process.env["PORT"];
+
+app.use(morgan("tiny"));
+app.use(express.static("./public"));
+
+app.get("/data", (request, response) => {
+    response.status(200).json(JSON.parse(fs.readFileSync("./public/data/app_posts.json", "utf-8")))
+});
+
+app.listen(port, () => {
+    console.log(`\u001B[92mListening to Port ${port}\u001B[39m`);
+});
